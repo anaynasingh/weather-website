0cfda5c9a69f236f25715204940b73559b7cfa74
Anayna <62555217+anaynasingh@users.noreply.github.com>
2020-07-23 14:46:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let person ={
+    name: 'Nidhi',
+    age:20
+}
+console.log(person);
+
+//Dot notation
+person.name="Ram";
+console.log(person.name);
+
+//bracket notation
+person['name']='Shastri';
+console.log(person.name);
+
+let selectedColors=['green','blue'];
+console.log(selectedColors[1]);
+
+var names=["anayna","nidhi","singh"]
